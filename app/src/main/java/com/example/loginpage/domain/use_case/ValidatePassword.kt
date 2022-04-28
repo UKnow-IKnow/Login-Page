d401a355ad5f73ca2b588dd60dcf8b7d69393f58
UKnow-IKnow <sarojsahu014@gmail.com>
2022-04-28 08:07:31 +0530

    add fun for validate password

---
+++ app/src/main/java/com/example/loginpage/domain/use_case/ValidatePassword.kt
@@ -0,0 +1,24 @@
+package com.example.loginpage.domain.use_case
+
+import android.util.Patterns
+
+class ValidatePassword {
+    fun execute(password: String): ValidateResult {
+        if (password.length < 8) {
+            return ValidateResult(
+                success = false,
+                errorMessage = "Password need to 8 character"
+            )
+        }
+        val containCharacterAndDigits = password.any { it.isDigit() } && password.all { it.isLetter() }
+        if (!containCharacterAndDigits) {
+            return ValidateResult(
+                success = false,
+                errorMessage = "The password need to contain atLeast one digit and character"
+            )
+        }
+        return ValidateResult(
+            success = true
+        )
+    }
+}
