1ab30bf5089d7637f3696181487ec5cdbd30c1a1
UKnow-IKnow <sarojsahu014@gmail.com>
2022-04-28 08:51:39 +0530

    added login form event

---
+++ app/src/main/java/com/example/loginpage/presentation/LoginFormEvent.kt
@@ -0,0 +1,10 @@
+package com.example.loginpage.presentation
+
+sealed class LoginFormEvent {
+    data class EmailChanged(val email: String) : LoginFormEvent()
+    data class PasswordChanged(val password: String) : LoginFormEvent()
+    data class RepeatedPasswordChanged(val repeatedPassword: String) : LoginFormEvent()
+    data class AcceptTerms(val isAccepted: String) : LoginFormEvent()
+
+    object Submit: LoginFormEvent()
+}
--- app/src/main/java/com/example/loginpage/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/loginpage/presentation/MainViewModel.kt
@@ -4,7 +4,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.loginpage.domain.use_case.ValidateEmail
+import com.example.loginpage.domain.use_case.ValidatePassword
+import com.example.loginpage.domain.use_case.ValidateRepeatedPassword
+import com.example.loginpage.domain.use_case.ValidateTerms
 
-class MainViewModel: ViewModel() {
+class MainViewModel(
+    private val validateEmail: ValidateEmail = ValidateEmail(),
+    private val validatePassword: ValidatePassword = ValidatePassword(),
+    private val validateRepeatedPassword: ValidateRepeatedPassword = ValidateRepeatedPassword(),
+    private val validateTerms: ValidateTerms = ValidateTerms()
+): ViewModel() {
     var state by mutableStateOf(LoginFormState())
+
+
 }
