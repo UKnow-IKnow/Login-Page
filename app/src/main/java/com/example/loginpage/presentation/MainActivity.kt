bf5a43653a730a0806676e31e2d8dac1a2bd9d04
UKnow-IKnow <sarojsahu014@gmail.com>
2022-04-29 10:17:53 +0530

    start with UI

--- app/src/main/java/com/example/loginpage/presentation/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/presentation/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.loginpage.presentation
 
 import android.os.Bundle
+import android.widget.Space
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshots.SnapshotApplyResult
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.loginpage.ui.theme.LoginPageTheme
+import kotlinx.coroutines.flow.collect
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +32,113 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val viewModel = viewModel<MainViewModel>()
+                    val state = viewModel.state
+                    val context = LocalContext.current
+                    LaunchedEffect(key1 = context) {
+                        viewModel.valudationEvents.collect { event ->
+                            when (event) {
+                                is MainViewModel.ValidationEvent.Success -> {
+                                    Toast.makeText(
+                                        context,
+                                        "Registration Successful",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                            }
+                        }
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(32.dp),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        TextField(
+                            value = state.email,
+                            onValueChange = {
+                                viewModel.onEvent(LoginFormEvent.EmailChanged(it))
+                            },
+                            isError = state.emailError != null,
+                            modifier = Modifier.fillMaxWidth(),
+                            placeholder = {
+                                Text(text = "Email")
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Email
+                            )
+                        )
+                        if (state.emailError != null) {
+                            Text(
+                                text = state.emailError,
+                                color = MaterialTheme.colors.error
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        TextField(
+                            value = state.password,
+                            onValueChange = {
+                                viewModel.onEvent(LoginFormEvent.PasswordChanged(it))
+                            },
+                            isError = state.passwordError != null,
+                            modifier = Modifier.fillMaxWidth(),
+                            placeholder = {
+                                Text(text = "Password")
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Password
+                            ),
+                            visualTransformation = PasswordVisualTransformation()
+                        )
+                        if (state.passwordError != null) {
+                            Text(
+                                text = state.passwordError,
+                                color = MaterialTheme.colors.error
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        TextField(
+                            value = state.repeatedPassword,
+                            onValueChange = {
+                                viewModel.onEvent(LoginFormEvent.RepeatedPasswordChanged(it))
+                            },
+                            isError = state.repeatedPasswordError != null,
+                            modifier = Modifier.fillMaxWidth(),
+                            placeholder = {
+                                Text(text = "Repeated Password")
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Password
+                            ),
+                            visualTransformation = PasswordVisualTransformation()
+                        )
+                        if (state.repeatedPasswordError != null) {
+                            Text(
+                                text = state.repeatedPassword,
+                                color = MaterialTheme.colors.error
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Checkbox(
+                                checked = state.acceptedTerms,
+                                onCheckedChange = {
+                                    viewModel.onEvent(LoginFormEvent.AcceptTerms(it))
+                                }
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(text = "Accepted Terms")
+                        }
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LoginPageTheme {
-        Greeting("Android")
-    }
-}
